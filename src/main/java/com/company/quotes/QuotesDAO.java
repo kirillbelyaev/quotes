8de887351080b836d2e39b14d563c46104410017
kirill <kirill@myasusbook>
2023-12-09 14:51:03 +0500

    refactoring

--- src/main/java/com/company/quotes/QuotesDAO.java
+++ src/main/java/com/company/quotes/QuotesDAO.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.company.quotes;
 
-import com.company.quotes.Constants.RETURN_CODES;
 import java.util.HashMap;
 import java.util.Map;
 
